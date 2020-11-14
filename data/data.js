e3c46cb6490262e53792ff12b3cbc68ea16c5501
carengyl <74403654+carengyl@users.noreply.github.com>
2020-11-14 18:30:20 +0900

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,7 @@
+var test = "Умножение";
+var vopros_1 = "4*12 + 7 = ";
+var vopros_2 = "7-8*2 = ";
+var vopros_3 = "34*2 + 17 = ";
+var pr_otv_zadachi_1 = "NTU="; 
+var pr_otv_zadachi_2 = "LTk=";
+var pr_otv_zadachi_3 = "ODU=";
